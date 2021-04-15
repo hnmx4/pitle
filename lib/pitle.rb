f98238499a9be1bbe3add387f2eb2e4e922df18e
hnmx4 <hnmnty@gmail.com>
2021-04-15 22:55:29 +0900

    extract logic from the class of CLI interface.

--- lib/pitle.rb
+++ lib/pitle.rb
@@ -1,13 +1,50 @@
 # frozen_string_literal: true
 
 require_relative "pitle/version"
-
-require 'pitle'
 require 'thor'
 
 module Pitle
   class Error < StandardError; end
   # Your code goes here...
+
+  class FileNameOperator
+    def initialize(filenames, extensions)
+      @filenames = filenames
+      @extensions = extensions
+    end
+
+    def collect_duplicate_files()
+      formatted_files = @filenames.each.map { |x| Util.divide_extension(x, @extensions) }
+
+      res = {}
+      formatted_files.each do |book|
+        name, ext = book
+        if res.key?(name) then
+          res[name].append(ext)
+        else
+          res[name] = [ext]
+        end
+      end
+
+      return res
+    end
+
+    def pick_files()
+      collected_files = collect_duplicate_files()
+
+      res = []
+      collected_files.each do |k, v|
+        @extensions.each do |e|
+          unless v.index(e).nil?
+            res.append("#{k}.#{v[v.index(e)]}")
+            break
+          end
+        end
+      end
+
+      return res
+    end
+  end
 
   class Util
     def self.divide_extension(filename, extensions)
@@ -24,56 +61,15 @@ module Pitle
 
   class CLI < Thor
     desc "hoge", "fuga"
-    def do(priority="epub,pdf,mobi")
-      books_sample = [
-        "CleanAgile-1.0.0.epub",
-        "CleanAgile-1.0.0.mobi",
-        "CleanArchitecture-1.0.0.epub",
-        "CleanArchitecture-1.0.0.mobi",
-        "CleanCode-1.0.0.epub",
-        "CleanCode-1.0.0.mobi",
-        "CleanCoder-1.0.0.epub",
-        "CleanCoder-1.0.0.mobi",
-        "GallupReport-1.pdf",
-        "GallupReport-2.pdf",
-        "LINUX ハンドブック.pdf",
-        "Land of Lisp.pdf",
-        "Real World HTTP 第2版.epub",
-        "Real World HTTP 第2版.mobi",
-        "SRE サイトリライアビリティエンジニアリング.epub",
-        "SRE サイトリライアビリティエンジニアリング.mobi",
-        "SRE サイトリライアビリティエンジニアリング.pdf",
-      ]
+    def do(extension="epub,pdf,mobi")
+      extensions = extension.split(',')
 
-      priorities = priority.split(',')
+      Dir.open(Dir.pwd){ |d|
+        fileNameOperator = FileNameOperator.new(d.children, extensions)
+        origin_filenames = fileNameOperator.pick_files()
 
-      formatted_books = books_sample.each.map { |x| Util.divide_extension(x, priorities) }
-
-      duplicate_books = {}
-      formatted_books.each do |book|
-        name, ext = book
-        if duplicate_books.key?(name) then
-          duplicate_books[name].append(ext)
-        else
-          duplicate_books[name] = [ext]
-        end
-      end
-
-      origin_books = []
-      duplicate_books.each do |k, v|
-        priorities.each do |e|
-          unless v.index(e).nil?
-            origin_books.append("#{k}.#{v[v.index(e)]}")
-            break
-          end
-        end
-      end
-
-      p origin_books
-
-      # Dir.open(Dir.pwd){ |d|
-      #   p d.children
-      # }
+        p origin_filenames
+      }
     end
 
     default_task :do
